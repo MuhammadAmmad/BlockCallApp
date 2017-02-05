cb88c88f9f811f97b3771c3a288aea4234a2ad72
Phuongpham09 <phuong.pham@nguonviet.vn>
2017-02-05 13:46:18 +0700

    delete something not necessary

--- app/src/main/java/com/example/phuong/blockcallapp/utils/Constant.java
+++
@@ -1,9 +0,0 @@
-package com.example.phuong.blockcallapp.utils;
-
-/**
- * Created by phuong on 05/02/2017.
- */
-
-public class Constant {
-    public static final String REQUEST_LIST_CONTACT = "list_contact";
-}
