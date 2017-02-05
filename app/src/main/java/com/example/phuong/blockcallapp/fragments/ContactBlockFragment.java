9179f141f8734d2173f8c0bae93834cd4185bf70
Phuongpham09 <phuong.pham@nguonviet.vn>
2017-02-05 22:55:41 +0700

    finish search feature

--- app/src/main/java/com/example/phuong/blockcallapp/fragments/ContactBlockFragment.java
+++ app/src/main/java/com/example/phuong/blockcallapp/fragments/ContactBlockFragment.java
@@ -2,16 +2,20 @@ package com.example.phuong.blockcallapp.fragments;
 
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
+import android.widget.EditText;
 
 import com.example.phuong.blockcallapp.R;
 import com.example.phuong.blockcallapp.adapters.ListContactBlockAdapter;
 import com.example.phuong.blockcallapp.models.ContactBlock;
+import com.example.phuong.blockcallapp.utils.Constant;
 
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.TextChange;
 import org.androidannotations.annotations.ViewById;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -21,6 +25,8 @@ import java.util.List;
 public class ContactBlockFragment extends BaseFragment {
     @ViewById(R.id.recyclerViewBlock)
     RecyclerView mRecyclerViewBlock;
+    @ViewById(R.id.edtSearch)
+    EditText mEdtSearch;
     private ListContactBlockAdapter mAdapter;
     private List<ContactBlock> mContactBlocks;
 
@@ -29,6 +35,11 @@ public class ContactBlockFragment extends BaseFragment {
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
         mRecyclerViewBlock.setLayoutManager(layoutManager);
         getDataBlockContact();
+        Collections.sort(mContactBlocks, new Comparator<ContactBlock>() {
+            public int compare(ContactBlock v1, ContactBlock v2) {
+                return v1.getName().compareTo(v2.getName());
+            }
+        });
         mAdapter = new ListContactBlockAdapter(mContactBlocks, getContext());
         mRecyclerViewBlock.setAdapter(mAdapter);
     }
@@ -36,6 +47,26 @@ public class ContactBlockFragment extends BaseFragment {
     public void getDataBlockContact() {
         mContactBlocks = new ArrayList<>();
         mContactBlocks = ContactBlock.listAll(ContactBlock.class);
-        Log.d("tag11", mContactBlocks.size() + " 123 ");
     }
+
+    @TextChange(R.id.edtSearch)
+    void onTextChangesSearch(CharSequence query) {
+        query = query.toString().toLowerCase();
+        final List<ContactBlock> filteredList = new ArrayList<>();
+
+        for (int i = 0; i < mContactBlocks.size(); i++) {
+
+            final String text = mContactBlocks.get(i).getName().toLowerCase();
+
+            if (Constant.unAccent(text).contains(Constant.unAccent(query.toString()))) {
+                filteredList.add(mContactBlocks.get(i));
+            }
+        }
+
+        mRecyclerViewBlock.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mAdapter = new ListContactBlockAdapter(filteredList, getActivity());
+        mRecyclerViewBlock.setAdapter(mAdapter);
+        mAdapter.notifyDataSetChanged();
+    }
+
 }
