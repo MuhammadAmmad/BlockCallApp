f50d872eafbb82b8fcae55de21d6bd773fb67636
at-phuongpham <phuong.pham@asiantech.vn>
2017-06-25 16:18:30 +0700

    delete method in main activity

--- app/src/main/java/com/example/phuong/blockcallapp/ui/welcome/MainActivity.java
+++ app/src/main/java/com/example/phuong/blockcallapp/ui/welcome/MainActivity.java
@@ -1,9 +1,7 @@
 package com.example.phuong.blockcallapp.ui.welcome;
 
-import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
-import android.util.Log;
 
 import com.example.phuong.blockcallapp.R;
 import com.example.phuong.blockcallapp.adapters.TabAdapter;
@@ -37,18 +35,5 @@ public class MainActivity extends BaseActivity {
         adapter.addFragment(ContactBlockFragment_.builder().build(), title[1]);
         adapter.addFragment(SettingsFragment_.builder().build(), title[2]);
         viewPager.setAdapter(adapter);
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        Log.d(getClass().getSimpleName(),"==========");
-//        List<Fragment> fragments = getSupportFragmentManager().getFragments();
-//        if (fragments != null) {
-//            for (Fragment fragment : fragments) {
-//                fragment.onRequestPermissionsResult(requestCode, permissions, grantResults);
-//            }
-//        }
-
     }
 }
